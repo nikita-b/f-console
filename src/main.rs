4da1e193d5fb80b9a8f20b247dccf9c66eb90e2c
Nikita Beletsky <nikita@belecky.ru>
2016-12-20 01:55:56 +0300

    add in/out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Write, Read, BufReader, BufRead};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut rstream = BufReader::new(stream.try_clone().unwrap());
+    let mut buffer = String::new();
+    //try!(stream.read_to_end(&mut buffer));
+    stream.write(b"Welcome to F-Console, Stranger!\nEnter password:").unwrap();
+    rstream.read_line(&mut buffer).unwrap();
+    //#stream.read()
+    //let mut name : ~str = ss.read_line().unwrap();
+    //stream.write(b"").unwrap();
+    //stream.write_str(format!("Hello, {}!\n", name));
+}
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+             }
+            Err(e) => { 
+                println!("Connection failed: {}", e);
+            }
+        }
+    }
 }
