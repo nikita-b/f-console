ffe1b04a7d66fdadc3bcfcf839d1656362ca2ed2
Nikita Beletsky <nikita@belecky.ru>
2016-12-22 00:47:27 +0300

    add check password

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Read, BufReader, BufRead};
 
+
+fn check_password(password: String) -> bool {
+    let PASSWORD = String::from("12345");
+    if password.trim() == PASSWORD {
+        return true;
+    }
+    false
+}
+
+
 fn handle_client(mut stream: TcpStream) {
     let mut rstream = BufReader::new(stream.try_clone().unwrap());
     let mut buffer = String::new();
-    //try!(stream.read_to_end(&mut buffer));
+
     stream.write(b"Welcome to F-Console, Stranger!\nEnter password:").unwrap();
     rstream.read_line(&mut buffer).unwrap();
-    //#stream.read()
-    //let mut name : ~str = ss.read_line().unwrap();
-    //stream.write(b"").unwrap();
-    //stream.write_str(format!("Hello, {}!\n", name));
+    match check_password(buffer) {
+        false => {
+            println!("Password correct!");
+        }
+        true  => {
+            println!("Nope");
+        }
+    }
 }
 
 fn main() {
